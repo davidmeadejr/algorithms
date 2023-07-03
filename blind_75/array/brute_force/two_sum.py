170aecdec1d7ad0ee64236eff6a8bf0c36d01f80
David Meade Jr <davidmeadejnr@gmail.com>
2023-07-04 00:52:01 +0100

    Updated two_sum.py brute force solution.

--- blind_75/array/brute_force/two_sum.py
+++ blind_75/array/brute_force/two_sum.py
@@ -1,11 +1,9 @@
 """ 
-Given an array of integers "nums" and an integer "target", return indices of the two numbers 
-such that they add up to the "target".
+Given an array of integers "nums" and an integer "target", return indices of the two numbers such that they add up to the "target".
 """
 
 """
-You may assume that each input would have exactly one solution, and you may not use the 
-the same element twice.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 """
 
 # You can return the answer in any order
@@ -18,11 +16,13 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
 
 """
+Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
 """
 
 """
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 """
@@ -32,3 +32,19 @@ def twoSum(nums, target):
         for j in range(i + 1, lens(nums)):
             if nums[i] + num[j] == target:
                 return [i, j]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
