15258688f1d0f13a605abc4daff0304eab4e9ab7
davidmeadejr <davidmeadejnr@gmail.com>
2024-10-29 23:54:13 +0000

    Updated Binary Search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -11,7 +11,7 @@ Time Complexity Explanation: If each iteration divides the input size in half (e
 # Implementation
 def binary_search(list, item):
     low = 0
-    high = len(list) - 2
+    high = len(list) - 1
 
     while low <= high:
         mid = (low + high) // 2
