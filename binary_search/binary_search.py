3735dd5d75bce27bb2acfabdb03c66c5d3004365
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-01 08:18:30 +0000

    Binary Search daily spaced repetition

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -14,14 +14,13 @@ def binary_search(my_list, item):
     high = len(my_list) - 1
 
     while low <= high:
-        mid = (low + high) // 2
+        mid =  (low + high) // 2
         guess = my_list[mid]
-        
         if guess == item:
             return mid
         if guess > item:
             high = mid - 1
-        if guess < item:
+        else:
             low = mid + 1
     return None
 
@@ -31,4 +30,3 @@ my_list = [1, 3, 5, 7, 9]
 print(binary_search(my_list, 3)) # => 1
 
 print(binary_search(my_list, -1)) # => none
-
