f912b555f1bb2e68bf8db90a2b9dc2e32787cb1e
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-03 20:49:09 +0000

    quicksort algorithim implemented

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -7,3 +7,19 @@ Time Complexity:
 
 Time Complexity Explanation: 
 """
+
+arr = [11, 23, 46, 57, 9, 14, 233, 6, 4, 35]
+
+def quicksort(arr):
+  if len(arr) < 2:
+    return arr
+  else:
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i <= pivot]
+    more = [i for i in arr[1:] if i > pivot]
+
+  return quicksort(less) + [pivot] + quicksort(more)
+
+print(quicksort(arr))
+
+
