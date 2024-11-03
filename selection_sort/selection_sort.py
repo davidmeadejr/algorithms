76ed8c5373a86bce450bef57aa4daf658a286fa8
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-03 05:48:43 +0000

    selection sort implemented and recursion exercises started.

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -12,6 +12,7 @@ Time Complexity Explanation: For each element you touch all elements again.
 def find_smallest(arr):
 	smallest = arr[0]
 	smallest_index = 0
+	
 	for i in range(1, len(arr)):
 		if arr[i] < smallest:
 			smallest = arr[i]
@@ -21,6 +22,7 @@ def find_smallest(arr):
 # Use the selection sort algorithm to sort an array in ascending order
 def selection_sort(arr):
 	new_arr = []
+	
 	for i in range(len(arr)):
 		smallest = find_smallest(arr)
 		new_arr.append(arr.pop(smallest))
