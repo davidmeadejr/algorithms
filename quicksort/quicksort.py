94bca509d3962a181fc17cd98ac453e69c7b4070
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-04 08:40:43 +0000

    updated quicksort explanation and created bfs file.

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,11 +1,12 @@
 """
 Pattern: Divide and Conquer + Recursion
 
-Solution:
+Solution: Define a base case that returns the simplest instance of the problem to stop the recursion, and then implement the recursive case where the function calls itself with a modified argument to gradually approach the base case.
 
-Time Complexity: 
+Time Complexity: Depends on the pivot used (see below)
+- Worst Case: O(n^2) happens when the pivot consistently selects the smallest or largest elements, leading to unbalanced partitions.
+- Average Case: O(n log n) occurs when the pivot splits the array into two approximately equal parts.
 
-Time Complexity Explanation: 
 """
 
 arr = [11, 23, 46, 57, 9, 14, 233, 6, 4, 35]
