5fb4bb60049e46ee6af56ad6b388e490c690e678
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-04 05:08:40 +0000

    basic algorithms implemented

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -9,13 +9,17 @@ Time Complexity Explanation: If each iteration divides the input size in half (e
 """
 
 # Implementation
+# Example 
+my_list = [1, 3, 5, 7, 9]
+
 def binary_search(my_list, item):
     low = 0
     high = len(my_list) - 1
 
     while low <= high:
-        mid =  (low + high) // 2
+        mid = (low + high) // 2
         guess = my_list[mid]
+
         if guess == item:
             return mid
         if guess > item:
@@ -23,9 +27,6 @@ def binary_search(my_list, item):
         else:
             low = mid + 1
     return None
-
-# Example 
-my_list = [1, 3, 5, 7, 9]
 
 print(binary_search(my_list, 3)) # => 1
 
